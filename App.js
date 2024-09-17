ed565cb9febbabddd8de4f7bdd2a8980df5e2876
Kevin Simpson <kevinsimpson@Kevins-MacBook-Pro.local>
2024-09-17 12:41:29 -0600

    Update endpoint

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
   useEffect(() => {
     const fetchWorkout = async () => {
       try {
-        const response = await fetch("http://10.0.0.86:3000/");
+        const response = await fetch("https://kickbox-wod-api-0cb570c9519f.herokuapp.com/");
         const parsedWorkout = await response.json();
         setWorkout(parsedWorkout);
       } catch (e) {
